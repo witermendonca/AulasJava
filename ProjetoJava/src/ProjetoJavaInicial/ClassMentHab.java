7953e6bbf37b0263f5440ce099b0e39b985dfb65
Witer <witer_m19@hotmail.com>
2020-11-12 15:57:13 -0300

    Projeto de introdução java

---
+++ ProjetoJava/src/ProjetoJavaInicial/ClassMentHab.java
@@ -0,0 +1,17 @@
+package ProjetoJavaInicial;
+
+import java.util.Scanner;
+public class ClassMentHab {
+
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("As habilidades que mais utilizei para chegar ate aqui foram "
+				+ "\nAs de Atenção aos Detalhes e Trabalho em Equipe."
+				+ "\nJá as Mentalidades que mais utilizei foram"
+				+ "\nAs de Responsabilidade Pessoal e de Mentalidade de Crescimento.");
+
+	}
+
+}
---
+++ ProjetoJava/src/ProjetoJavaInicial/ClassSobreMim.java
@@ -0,0 +1,17 @@
+package ProjetoJavaInicial;
+
+import java.util.Scanner;
+public class ClassSobreMim {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+			
+		System.out.println("Ola me chamo Witer Xavier Mendonça,"
+				+ "\ndentro dos meus hobbies está desenhar, escrever, esportes, pratico Muay Thai e JiuJitsu."
+				+ "\nNas minhas horas vagas tento descansar e dividir meu tempo entre meus hobbies e família e minha namorada."
+				+ "\nGosto de trabalhar, viajar, de programação, trocar experiencias com pessoas. "
+				+ "\nJá algumas coisas que não gosto é falta de pontualidade, falta de empatia , berinjela. ");
+	}
+}
